99a497c78530e904d73b0fcdc9a8c316a999e520
Ashutosh Jaiswal <7.ashutoshj@gmail.com>
2024-06-05 23:43:50 +0530

    sde sheet array part1: 76 set matrix zeros

--- CSES Problems/ashu.py
+++
@@ -1,5 +0,0 @@
-class CS_Student:
-    stream = 'cse'                  # Class Variable
-    def __init__(self,name,roll):
-        self.name = name            # Instance Variable
-        self.roll = roll            # Instance Variable
