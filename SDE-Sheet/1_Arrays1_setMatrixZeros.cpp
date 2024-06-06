2eaa31c90b3563a54dbfb360b899357981275786
Ashutosh Jaiswal <7.ashutoshj@gmail.com>
2024-06-06 10:16:16 +0530

    Arrays P1: LC#76 Set Matrix Zeros

--- SDE-Sheet/1_Arrays1_setMatrixZeros.cpp
+++ SDE-Sheet/1_Arrays1_setMatrixZeros.cpp
@@ -34,24 +34,102 @@ Example 2:
 
 class Solution {
     public:
-    static void setZeroes(vector<vector<int>>& matrix) {
-        vector<pair<int, int>> zeros;
+    void markRowZero(vector<vector<int>>& matrix, int i) {
+        for (int col = 0; col < matrix[0].size(); col++) {
+            if (matrix[i][col] != 0)
+                matrix[i][col] = -1;
+        }
+
+    } 
+    void markColZero(vector<vector<int>>& matrix, int j) {
         for (int row = 0; row < matrix.size(); row++) {
-            for (int col = 0; col < matrix[0].size(); col++) {
-                if (matrix[row][col] == 0) {
-                    zeros.push_back({row, col});
+            if (matrix[row][j] != 0)
+                matrix[row][j] = -1;
+        }
+    } 
+    void setZeroes1(vector<vector<int>>& matrix) {
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix[0].size(); j++) {
+                if (matrix[i][j] == 0) {
+                    markRowZero(matrix, i);
+                    markColZero(matrix, j);
                 }
             }
         }
-        
-        for (int i = 0; i < zeros.size(); i++) {
-            for (int row = 0; row < matrix.size(); row++) {
-                for (int col = 0; col < matrix[0].size(); col++) {
-                    if (row == zeros[i].first || col == zeros[i].second) {
-                        matrix[row][col] = 0;
-                    }
+
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix[0].size(); j++) {
+                if (matrix[i][j] == -1) {
+                    matrix[i][j] = 0;
                 }
             }
+        }
+
+    }
+
+    void setZeroes2(vector<vector<int>>& matrix) {
+        // 2. traverse matrix and if matrix[i][j] == 0 then mark row[i] & col[j]
+        // traverse again and check if row[i] || col[j] has marked element then set matrix[i][j] = 0;
+        // TC : O(2* m*n) | SC : O(m+n)
+
+        vector<int> row (matrix.size(), 0);
+        vector<int> col (matrix[0].size(), 0);
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix[0].size(); j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix[0].size(); j++) {
+                if (row[i]  == 1 || col[j] == 1) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    void setZeroes3(vector<vector<int>>& matrix) {
+        // 3. traverse matrix and 
+        // if matrix[i][j] == 0 then mark 
+        // row[i] -> matrix[..][0] & 
+        // col[j] -> matrix[0][..]
+        // traverse once again for i,j >= 1 and check if matrix[..][0] || matrix[0][..] has marked element(0) 
+        // then set matrix[i][j] = 0;
+            // Again 1st traverse 0th row and mark each element 0 if matrix[0][0] has 0
+            // Again next traverse 0th col and mark each element 0 if col0 has 0
+        // TC : O(2n) | SC : O(1)
+
+        int col0 = 1;
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix[0].size(); j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0; // row[i] = 1
+                    if (j != 0)
+                        matrix[0][j] = 0; // col[j] = 1
+                    else
+                        col0 = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < matrix.size(); i++) {
+            for (int j = 1; j < matrix[0].size(); j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) { // (row[i] || col[j]) == 1
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < matrix[0].size(); j++)
+                matrix[0][j] = 0;
+        }
+        if (col0 == 0) {
+            for (int i = 0; i < matrix.size(); i++)
+                matrix[i][0] = 0;
         }
     }
 };
@@ -59,31 +137,38 @@ class Solution {
 
 int main() 
 {
-    vector<vector<int>> matrix1 = {{1,1,1},{1,0,1},{1,1,1}};
-    vector<vector<int>> matrix2 = {{0,1,2,0},{3,4,5,2}};
-    Solution::setZeroes(matrix1);
-    Solution::setZeroes(matrix2);
+    Solution sol;
 
+    // Input Initialization
+    vector<vector<int>> matrix1 = {{0,1,2,8},{3,4,5,2},{0,1,2,9},{3,4,5,2},{0,1,2,9}};
+    vector<vector<int>> matrix2 = {{0,1,2,8},{3,4,5,2},{0,1,2,9},{3,4,5,2},{0,1,2,9}};
+    vector<vector<int>> matrix3 = {{0,1,2,8},{3,4,5,2},{0,1,2,9},{3,4,5,2},{0,1,2,9}};
+
+    // Method Invocation
+    sol.setZeroes1(matrix1);
+    sol.setZeroes2(matrix2);
+    sol.setZeroes3(matrix3);
+
+    // Result Visualization
     printMatrix(matrix1);
     printMatrix(matrix2);
+    printMatrix(matrix3);
 
     return 0;
 }
 
 /*
-Solution Explaination Here:
+SOLUTIONS:-
 
-Solutions:-
-
-1. O(1) Space & O(n^3) TC: 
+1. O(1) Space & O(n^3) TC: Brute Force
 traverse each element of matrix and for each element, 
     a. if element == 0, convert each corresponding element in that row & column from 1 -> -1 (exclude converting 0 -> -1).
     b. reiterate entire matrix  and now convert those -1 -> 0.
 
-2. O(n+m) Space & O(n^2) TC: 
+2. O(n+m) Space & O(n^2) TC: Optimized
 have a zeroRow and zeroCol arrays of size n & m. Populate zeroRow[i] and zeroCol[j] of these arrays if matrix[i][j] == 0.  Set elements of orignal matrix as zero according to the two arrays.
 
-3. O(1) extra space & 2O(n^2) TC:
+3. O(1) extra space & O(n^2) TC: OPTIMIZED
 Inplace store row/col to be set as zero by marking 0 in 0th row and 0th col.
     Keep extra variable for the overlapping matrix[0][0] element as Col0 and mark it initaially as 1.
     mark all inplace the zeros in 0th col and 0th row.
