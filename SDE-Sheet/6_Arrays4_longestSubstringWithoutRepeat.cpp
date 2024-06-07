0572f4993f74e1630b5bb13363ac13fdb7c0287b
Ashutosh Jaiswal <7.ashutoshj@gmail.com>
2024-06-07 23:01:53 +0530

    Arrays P1: LC#3. Longest Substring Without Repeating Characters

--- SDE-Sheet/6_Arrays4_longestSubstringWithoutRepeat.cpp
+++ SDE-Sheet/6_Arrays4_longestSubstringWithoutRepeat.cpp
@@ -160,13 +160,38 @@ int main()
 SOLUTIONS:-
 
 1. O(N) Space & O(n^2) TC | Brute Force
+    - in a nested for loop with i->n of outer loop, find all sub strings starting from i
+        - inner loop will run from j = i+1 to n and find all sub-strings starting from i and ending at j
+        - maintain hashset and if s[j] is not present in hashset
+            - increment curr_len
+            - also insert s[i] in hashset since it was not present currently and continue
+        - else if s[i] present in hashset then break
+        - in each inner loop, curr_len challanges max_len and if it is greater than max_len then update max_len
 
 
 2. O(N) Space & O(2N) TC | Optimized
+    - Maintain 2 pointers l,r = 0
+    - while r < n
+        - if s[r] is not found in hash_set
+            - insert s[r] in hash_set and inrement r
+        - else
+            -erase s[l] and increment l
+        - each time challange and increment max_sum if current_length of sub-array is greater than max_sum
 
 
 3.  O(N) extra space & O(N) TC | OPTIMIZED
+    - maintain a hash map of s[i],i where i demotes the index at which s[i] was last seen
+    - this is used for jumping directly to appropriate l position rather than moving incrementally
 
+    - Same as last code, take 2 ptrs left and right
+    - while right < n
+        - if s[right] is found in hash_map then 
+            - update left ptr such that if left is at valid position then update it else leave left as it is
+            - Valid: hash_map[s[right]] > left
+        - Update hash_map entry of s[right] = right;
+        - challange and increment max_sum if current_length of sub-array is greater than max_sum 
+            - max_len = max(max_len, right - left + 1)
+        - right++
 
 */
 
