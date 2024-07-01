e03a1c65198f98b0f4761609d782f89002c9637e
Ashutosh Jaiswal <7.ashutoshj@gmail.com>
2024-07-01 19:15:51 +0530

    18_Arrays3_reversePairs

--- SDE-Sheet/18_Arrays3_reversePairs.cpp
+++ SDE-Sheet/18_Arrays3_reversePairs.cpp
@@ -54,7 +54,72 @@ Constraints:
 
 class Solution {
     public:
-    PASTE_METHOD_DECLARATION_HERE
+    int reversePairs1(vector<int>&a) {
+        int cnt = 0, n = a.size();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (a[i] > 2 * a[j]) cnt++;
+            }
+        }
+        return cnt;
+    }
+
+    int countInverse(vector<int>& arr, int low, int mid, int high) {
+        int l1 = low, r1 = mid;
+        int l2 = mid+1, r2 = high;
+        int i = l1, j = l2, ans = 0;
+        while (i <= r1 && j <= r2) {
+            if ((long long)(arr[i]) > (long long)arr[j] * 2) {
+                ans += (r1 - i + 1);
+                j++;
+            }
+            else {
+                i++;
+            }
+        }
+        return ans;
+    }
+    void merge(vector<int>& arr, int low, int mid, int high) {
+        int l1 = low, r1 = mid;
+        int l2 = mid+1, r2 = high;
+
+        vector<int> temp;
+        while (l1 <= r1 && l2 <= r2) {
+            if (arr[l1] <= arr[l2])
+                temp.push_back(arr[l1++]);
+            else
+                temp.push_back(arr[l2++]);
+        }
+        while (l1 <= r1)
+            temp.push_back(arr[l1++]);
+        while (l2 <= r2)
+            temp.push_back(arr[l2++]);
+
+        for (int i = 0; i < high-low+1; i++) {
+            arr[low+i] = temp[i];
+        }
+    }
+    int mergeSort(vector<int>& arr, int low, int high) {
+        // Base Condition
+        if (low >= high) 
+            return 0;
+
+        // Calculate mid
+        int mid = low + ((high - low) / 2);
+        int ans = 0;
+
+        ans += mergeSort(arr, low, mid);
+        ans += mergeSort(arr, mid+1, high);
+        ans += countInverse(arr, low, mid, high);
+        merge(arr, low, mid, high);
+        return ans;
+    }
+    int reversePairs2(vector<int>& nums) {
+        int ans = 0;
+        ans = mergeSort(nums, 0, nums.size()-1);
+        // for (long long x : nums) cout << x << " ";
+        return ans;
+    }
 
 };
 
@@ -64,15 +129,16 @@ int main()
     Solution sol;
 
     // Input Initialization
+    vector<int> arr1 = {1, 3, 2, 3, 1};
+    vector<int> arr2 = {1, 3, 2, 3, 1, 4, 5, 2, 7, 8, 2, 1};
 
 
-    // Method Invocation
-    cout << sol.method_name(param1) << endl;
+    // Method Invocation & Result Visualization
+    cout << sol.reversePairs1(arr1) << endl;
+    cout << sol.reversePairs1(arr2) << endl;
 
-
-
-    // Result Visualization
-
+    cout << sol.reversePairs2(arr1) << endl;
+    cout << sol.reversePairs2(arr2) << endl;
 
 
     return 0;
