08301f9992a65fa7ebafd82495ce2d2d19d33603
Ashutosh Jaiswal <7.ashutoshj@gmail.com>
2024-07-05 02:00:04 +0530

    22_Arrays4_largestSubarrayWithSum

--- SDE-Sheet/22_Arrays4_largestSubarrayWithSum0.cpp
+++ SDE-Sheet/22_Arrays4_largestSubarrayWithSum0.cpp
@@ -50,6 +50,7 @@ Constraints:
 
 class Solution {
     public:
+    // Better || Most optimal in case array has +ve, -ve and 0s
     int maxLen1(vector<int>&A, int n) {   // Edge case approach 1
         unordered_map<int, int> sums; // prefixSum : index
         sums[0] = -1; // initalise our "sums" hash_map to contain initial value of prefixSum i.e. 0
@@ -67,6 +68,7 @@ class Solution {
         return ans;
     }
 
+    // Better || Most optimal in case array has +ve, -ve and 0s
     int maxLen2(vector<int>&A, int n) {   // Edge case approach2
         unordered_map<int, int> sums; // prefixSum : index
         int prefixSum = 0, ans = 0;
@@ -84,6 +86,24 @@ class Solution {
         return ans;
     }
 
+    // Optimal || Only works if array has 0s or +ve
+    int maxLen3(vector<int>&A, int k) {   // Edge case approach2
+        int l = 0, r = 0, sum = 0, n = A.size(), ans = 0;
+        while (l <= r && r < n) {
+            sum += A[r];
+            while (l <= r && sum > k) {
+                sum -= A[l];
+                l++;
+            }
+            if (sum == k) {
+                ans = max(ans, r - l + 1);
+            }
+            r++;
+        }
+        return ans;
+    }
+
+
 };
 
 
@@ -97,6 +117,7 @@ int main()
     vector<int> arr3 = {1, 0, -4, 3, 1, 0};
     vector<int> arr4 = {1, -1, 1, -1};
     vector<int> arr5 = {5,6,-1,5,-4,-2,-2,5,-1,0,1,1};
+    vector<int> arr6 = {1,2,3,1,1,1,1,3,3};
 
     // Method Invocation & Result Visualization
     cout << sol.maxLen1(arr1, 8) << endl;
@@ -111,6 +132,8 @@ int main()
     cout << sol.maxLen2(arr4, 4) << endl;
     cout << sol.maxLen2(arr5, 12) << endl;
 
+    cout << sol.maxLen3(arr2, 16) << endl;
+    cout << sol.maxLen3(arr6, 6) << endl;
 
     return 0;
 }
@@ -126,13 +149,10 @@ SOLUTIONS:-
     - if current subArray sum reaches 0 then challenge max_len 
     - return max_len 
 
+2. TC: O(n) | SC: O(n) | Better-edgeCase1*
+    < Better in case where array only has zeros and positive numbers >
+    < OPTIMIZED in case where array has negative numbers too >
 
-idx    ->   0*   1   2    3   4   5*  6   7  
-arr[]   =  [15, -2,  2,  -8,  1,  7,  4,  8]
-prefixSum = 15  13  15    7   8   15  19  27
-res=5-0=[5]     <------max_lex-----> 
-
-2. TC: O(n) | SC: O(n) | OPTIMIZED-edgeCase1*
     # hash map [prefixSum : idx] initialized with [sum:0, idx:-1] 
     - declare a unordered_map 'prefixSums' for [prefixSum : index]
     - *initialize 'currSum' as 0 and initially store prefixSum of '0' as '-1' since we initially start with sum 0 
@@ -144,7 +164,15 @@ res=5-0=[5]     <------max_lex----->
         - challenge and update max_len with curr_len at the end of each iteration if necessary
     - return max_len
 
-2.  TC: O(n) | SC: O(n) | OPTIMIZED-edgeCase2*
+                idx    ->   0*   1   2    3   4   5*  6   7  
+                arr[]   =  [15, -2,  2,  -8,  1,  7,  4,  8]
+                prefixSum = 15  13  15    7   8   15  19  27
+                res=5-0=[5]     <------max_lex-----> 
+
+2.  TC: O(n) | SC: O(n) | Better-edgeCase2
+    < Better in case where array only has zeros and positive numbers >
+    < OPTIMIZED in case where array has negative numbers too >
+
     # maintain hash_map [prefixSum : idx] return i+1 if prefixSum@i itself is 0
     - declare a unordered_map 'prefixSums' for [prefixSum : index]
     - iterate over the arr using for loop & calculate prefixSum by adding current element to sum
@@ -153,6 +181,21 @@ res=5-0=[5]     <------max_lex----->
         - curr_len will become curr_idx - index where curr_sum was previously encountered
         - challenge and update max_len with curr_len if necessary
     - else current value of prefixSum is seen first time -> add [prefixSum, idx] to hash map
+    - return max_len
+
+3.  TC: O(n) | SC: O(1) | OPTIMIZED-edgeCase2
+    < OPTIMIZED but works ONLY in case where array has zeros or positive numbers >
+
+    # 2 pointer sliding window
+    - initialize left, right, sum, ans as 0
+    - iterate over the array untill right is less than n and left is less than right
+        - add element at right to the sum
+        - if sum becomes larger than our target -> shorten the window size
+            - decrement from sum  by the element at left index 
+            - increase left index then again to check if there's still need to shorten the window size
+        - check if sum became equal to our target
+            - if yes, then update our max_len if curr window size (r-l+1) is max so far
+        - increase window size
     - return max_len
 */
 
